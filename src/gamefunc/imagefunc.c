f6415045203a556e5040d4c8e565398949e94b85
yrjoelae <martin.yrjola@gmail.com>
2009-02-23 19:01:22 +0200

    got something to show on screen

--- src/gamefunc/imagefunc.c
+++ src/gamefunc/imagefunc.c
@@ -10,3 +10,13 @@ SDL_Surface* load_image(const char* filename){
     return optimizedImage;
     }
 }
+
+void draw_surface(int x, int y, SDL_Surface* source, SDL_Surface* target){
+    /* Temporary rect for offset */
+    SDL_Rect temprect;
+    temprect.x = x;
+    temprect.y = y;
+    /* Blitting time */
+    SDL_BlitSurface(source, NULL, target, &temprect);
+}
+    
--- src/main.c
+++ src/main.c
@@ -3,9 +3,26 @@
 
 #include "SDL/SDL.h"
 
-SDL_Surface* screen = NULL;
+#include "gamefunc/imagefunc.h"
+
 
 int main(int argc, char** argv){
-    printf("hello world\n");
+    SDL_Surface* screen     = NULL;
+    SDL_Surface* background = NULL;
+    if (SDL_Init(SDL_INIT_EVERYTHING) == -1){
+        printf("Error: SDL didn't initialize\n");
+        return 1;
+    }
+    printf("SDL initialized\n");
+    screen = SDL_SetVideoMode(640, 480, 32, SDL_SWSURFACE);
+    background = load_image("data/kaboom.png");
+    if (background == NULL){
+        printf("image kaboom.png not found");
+        return 1;
+    }
+    draw_surface(0, 0, background, screen);
+    SDL_Flip(screen);
+    SDL_Delay(5000);
+    SDL_Quit();
     return 0;
 }

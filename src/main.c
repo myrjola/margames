4675842e79b66f4bbda2f884f99556309bbe2f84
yrjoelae <martin.yrjola@gmail.com>
2009-03-01 14:07:04 +0200

    Revert "got something to show on screen"
    This reverts commit f6415045203a556e5040d4c8e565398949e94b85.

--- src/main.c
+++ src/main.c
@@ -3,26 +3,9 @@
 
 #include "SDL/SDL.h"
 
-#include "gamefunc/imagefunc.h"
-
+SDL_Surface* screen = NULL;
 
 int main(int argc, char** argv){
-    SDL_Surface* screen     = NULL;
-    SDL_Surface* background = NULL;
-    if (SDL_Init(SDL_INIT_EVERYTHING) == -1){
-        printf("Error: SDL didn't initialize\n");
-        return 1;
-    }
-    printf("SDL initialized\n");
-    screen = SDL_SetVideoMode(640, 480, 32, SDL_SWSURFACE);
-    background = load_image("data/kaboom.png");
-    if (background == NULL){
-        printf("image kaboom.png not found");
-        return 1;
-    }
-    draw_surface(0, 0, background, screen);
-    SDL_Flip(screen);
-    SDL_Delay(5000);
-    SDL_Quit();
+    printf("hello world\n");
     return 0;
 }

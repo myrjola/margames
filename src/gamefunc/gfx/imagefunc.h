214f46168d61e9139b183d0b3254d498d9e70813
yrjoelae <martin.yrjola@gmail.com>
2009-03-02 13:06:18 +0200

    forgot ifndef, define in imagefunc.h

--- src/gamefunc/gfx/imagefunc.h
+++ src/gamefunc/gfx/imagefunc.h
@@ -2,6 +2,11 @@
 #include "SDL/SDL.h"
 #include "SDL/SDL_image.h"
 
+#ifndef IMAGEFUNC_H
+#define IMAGEFUNC_H
+
 SDL_Surface* load_image(const char* filename);
 
 void draw_surface(int x, int y, SDL_Surface* source, SDL_Surface* target);
+
+#endif // IMAGEFUNC_H
